1164693ab92eb404bfdfc21ace6ff0e33d137aef
Gearoid O Fearghail <gearoidof@gmail.com>
2019-09-15 11:38:59 +0100

    adding payment handling

--- web/js/payment.js
+++ web/js/payment.js
@@ -3,3 +3,25 @@ var stripe = Stripe('pk_test_8oWI4fD0zgVXlMJavJNJpaHk');
 var elements = stripe.elements();
 var cardElement = elements.create('card');
 cardElement.mount('#card-element');
+
+var cardholderName = document.getElementById('cardholder-name');
+var cardButton = document.getElementById('card-button');
+var clientSecret = cardButton.dataset.secret;
+
+cardButton.addEventListener('click', function(ev) {
+  console.log("click");
+  console.log(cardholderName.value);
+  stripe.handleCardPayment(
+    clientSecret, cardElement, {
+      payment_method_data: {
+        billing_details: {name: cardholderName.value}
+      }
+    }
+  ).then(function(result) {
+    if (result.error) {
+      // Display error.message in your UI.
+    } else {
+      // The payment has succeeded. Display a success message.
+    }
+  });
+});
